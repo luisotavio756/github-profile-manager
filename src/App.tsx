2a23dbeba413254e084689805c59c27005de9bda
Luis Otavio <luis.otavio2425@gmail.com>
2020-11-17 09:14:40 -0300

    chore: add App render routes structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import GlobalStyle from './styles/global';
 
-const App: React.FC = () => <h1>Green Mile</h1>;
+import Routes from './routes';
+
+import AppProvider from './hooks';
+
+const App: React.FC = () => (
+  <Router>
+    <AppProvider>
+      <Routes />
+    </AppProvider>
+    <GlobalStyle />
+  </Router>
+);
 
 export default App;
